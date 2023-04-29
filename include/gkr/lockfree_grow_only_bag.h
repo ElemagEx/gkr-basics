aca7db176b566d2601247368ba72c4065d7f30b7
Drago <drago@quakvr.io>
2023-04-30 01:47:11 +0300

    lockfree_grow_only_bag refined and its in progress

--- include/gkr/lockfree_grow_only_bag.h
+++ include/gkr/lockfree_grow_only_bag.h
@@ -19,13 +19,26 @@
 #error  You must use C++14 or preinclude implementation of std::exchange
 #endif
 
+#include <cassert>
+
 #ifndef DIAG_NOEXCEPT
 #define DIAG_NOEXCEPT true
 #endif
 #ifndef Assert_NotNullPtr
-#define Assert_NotNullPtr(cond)
+#define Assert_NotNullPtr(cond) assert(cond)
+#endif
+#ifndef Check_Recovery
+#define Check_Recovery(msg)
 #endif
 
+#endif
+
+#ifndef if_constexpr
+#ifdef __cpp_if_constexpr
+#define if_constexpr if constexpr
+#else
+#define if_constexpr if
+#endif
 #endif
 
 namespace gkr
@@ -34,57 +47,115 @@ namespace gkr
 template<typename T, typename Allocator=std::allocator<T>>
 class lockfree_grow_only_bag
 {
-    lockfree_grow_only_bag           (const lockfree_grow_only_bag&) noexcept = delete;
-    lockfree_grow_only_bag& operator=(const lockfree_grow_only_bag&) noexcept = delete;
+public:
+    using value_type      = T;
+    using reference       = T&;
+    using const_reference = const T&;
+
+    using difference_type = std::ptrdiff_t;
+    using size_type       = std::size_t;
 
 private:
-    struct node_t
+    struct node_type
     {
-        T       value {};
-        node_t* next  {nullptr};
+        value_type value {};
+        node_type* next  {nullptr};
 
-        node_t() noexcept = default;
-       ~node_t() noexcept = default;
+        node_type() noexcept(std::is_nothrow_default_constructible<value_type>::value) = default;
+       ~node_type() noexcept(std::is_nothrow_destructible         <value_type>::value) = default;
 
-        node_t           (node_t&&) noexcept = delete;
-        node_t& operator=(node_t&&) noexcept = delete;
+        node_type           (node_type&&) noexcept = delete;
+        node_type& operator=(node_type&&) noexcept = delete;
 
-        node_t           (const node_t&) noexcept = delete;
-        node_t& operator=(const node_t&) noexcept = delete;
+        node_type           (const node_type&) noexcept = delete;
+        node_type& operator=(const node_type&) noexcept = delete;
+
+        node_type(const value_type&  v) noexcept(std::is_trivially_copy_constructible<value_type>::value) : value(          v ) {}
+        node_type(      value_type&& v) noexcept(std::is_trivially_move_constructible<value_type>::value) : value(std::move(v)) {}
+
+        template<typename... Args>
+        node_type(Args&&... args) noexcept(std::is_nothrow_constructible<Args...>::value) : value(std::forward<Args>(args)...) {}
     };
 
-public:
-    using       value_type = T;
-    using   allocator_type = typename std::allocator_traits<Allocator>::template rebind_alloc<node_t>;
-
 private:
+    using allocator_type = typename std::allocator_traits<Allocator>::template rebind_alloc<node_type>;
+
     using allocator_traits = std::allocator_traits<allocator_type>;
 
-    static constexpr bool      type_is_supported = std::is_default_constructible<value_type>::value;
-    static constexpr bool allocator_is_supported =
-        allocator_traits::is_always_equal::value || (
-        allocator_traits::propagate_on_container_swap::value &&
-        allocator_traits::propagate_on_container_move_assignment::value
-        );
-    static_assert(     type_is_supported, "Only defaul-constructible value types are supported");
-    static_assert(allocator_is_supported, "Only always-equal or propagate-on move-assignment/swap allocators are supported");
-
 private:
-    std::atomic<node_t*> m_first {nullptr};
+    std::atomic<node_type*> m_first {nullptr};
 
     [[no_unique_address]]
     allocator_type m_allocator;
 
+private:
+    node_type* copy_elements(const node_type* other_first) noexcept(false)
+    {
+        node_type* new_first = nullptr;
+
+        for(const node_type* src_node = other_first; src_node != nullptr; src_node = src_node->next)
+        {
+            node_type* new_node = m_allocator.allocate(1);
+
+            new (new_node) node_type(src_node->value);
+
+            new_node->next = new_first;
+
+            new_first = new_node;
+        }
+        return new_first;
+    }
+    node_type* move_elements(node_type* other_first) noexcept(false)
+    {
+        node_type* new_first = nullptr;
+
+        for(const node_type* src_node = other_first; src_node != nullptr; src_node = src_node->next)
+        {
+            node_type* new_node = m_allocator.allocate(1);
+
+            new (new_node) node_type(std::move(src_node->value));
+
+            new_node->next = new_first;
+
+            new_first = new_node;
+        }
+        return new_first;
+    }
+
 public:
-    lockfree_grow_only_bag(Allocator allocator = Allocator()) noexcept(
-        std::is_nothrow_constructible<allocator_type, Allocator>::value
+    lockfree_grow_only_bag(const Allocator& allocator = Allocator()) noexcept(
+        std::is_nothrow_constructible<allocator_type, const Allocator&>::value
         )
         : m_allocator(allocator)
     {
     }
-    ~lockfree_grow_only_bag()
+    ~lockfree_grow_only_bag() noexcept(
+        std::is_nothrow_destructible<allocator_type>::value &&
+        std::is_nothrow_destructible<     node_type>::value
+        )
     {
         clear();
+    }
+
+    lockfree_grow_only_bag(const lockfree_grow_only_bag& other) noexcept(false)
+        : m_allocator(allocator_traits::select_on_container_copy_construction(other.m_allocator))
+    {
+        m_first = copy_elements(other.m_first);
+    }
+    lockfree_grow_only_bag& operator=(const lockfree_grow_only_bag& other) noexcept(false)
+    {
+        if(this != std::addressof(other))
+        {
+            clear();
+
+            if_constexpr(allocator_traits::propagate_on_container_copy_assignment::value)
+            {
+                m_allocator = allocator_traits::select_on_container_copy_construction(other.m_allocator);
+            }
+
+            m_first = copy_elements(other.m_first);
+        }
+        return *this;
     }
 
     lockfree_grow_only_bag(lockfree_grow_only_bag&& other) noexcept(
@@ -94,73 +165,123 @@ public:
         , m_allocator(std::move(other.m_allocator))
     {
     }
-    lockfree_grow_only_bag& operator=(lockfree_grow_only_bag&& other) noexcept
+    lockfree_grow_only_bag& operator=(lockfree_grow_only_bag&& other) noexcept(
+        std::is_nothrow_destructible<node_type>::value && (
+            allocator_traits::is_always_equal::value
+            ||
+            (allocator_traits::propagate_on_container_move_assignment::value &&
+            std::is_nothrow_move_constructible<allocator_type>::value))
+        )
     {
-        if(this != &other)
+        if(this != std::addressof(other))
         {
             clear();
 
-#ifdef __cpp_if_constexpr
-            if constexpr(allocator_traits::propagate_on_container_move_assignment::value)
-#else
-            if          (allocator_traits::propagate_on_container_move_assignment::value)
-#endif
+            if_constexpr(allocator_traits::propagate_on_container_move_assignment::value)
             {
                 m_allocator = std::move(other.m_allocator);
+
+                m_first = other.m_first.exchange(nullptr);
             }
-            m_first = other.m_first.exchange(nullptr);
+            else if_constexpr(allocator_traits::is_always_equal::value)
+            {
+                m_first = other.m_first.exchange(nullptr);
+            }
+            else if(m_allocator == other.m_allocator)
+            {
+                m_first = other.m_first.exchange(nullptr);
+            }
+            else
+            {
+                m_first = move_elements(other.m_first);
+
+                other.clear();
+            }
         }
         return *this;
     }
-    void swap(lockfree_grow_only_bag& other) noexcept
+public:
+    static constexpr bool swap_is_noexcept = (
+        allocator_traits::is_always_equal::value
+        ||
+        (allocator_traits::propagate_on_container_swap::value &&
+        std::is_nothrow_move_constructible<allocator_type>::value &&
+        std::is_nothrow_move_assignable   <allocator_type>::value )
+        );
+
+    void swap(lockfree_grow_only_bag& other) noexcept(swap_is_noexcept)
     {
-        if(this != &other)
+        if(this != std::addressof(other))
         {
-#ifdef __cpp_if_constexpr
-            if constexpr(allocator_traits::propagate_on_container_swap::value)
-#else
-            if          (allocator_traits::propagate_on_container_swap::value)
-#endif
+            if_constexpr(allocator_traits::propagate_on_container_swap::value)
             {
                 std::swap(m_allocator, other.m_allocator);
+
+                m_first = other.m_first.exchange(m_first);
             }
-            m_first = other.m_first.exchange(m_first);
+            else if_constexpr(allocator_traits::is_always_equal::value)
+            {
+                m_first = other.m_first.exchange(m_first);
+            }
+            else if(m_allocator == other.m_allocator)
+            {
+                m_first = other.m_first.exchange(m_first);
+            }
+            else
+            {
+                Check_Recovery(
+                    "According to the standard"
+                    " (see AllocatorAwareContainer at https://en.cppreference.com/w/cpp/named_req/AllocatorAwareContainer)"
+                    " this case is undefined behaviour."
+                    " Uneffective code follows"
+                    );
+                auto    first = this->move_elements(other.m_first);
+                other.m_first = other.move_elements(this->m_first);
+
+                clear();
+                m_first = first;
+            }
         }
     }
 
 public:
+    static size_type max_size() noexcept {
+        return static_cast<size_type>(-1) / sizeof(value_type);
+    }
+    size_type size() const noexcept
+    {
+        size_type size = 0;
+
+        for(node_type* node = m_first; node != nullptr; node = node->next) ++size;
+
+        return size;
+    }
     bool empty() const noexcept
     {
         return (m_first == nullptr);
     }
-    const allocator_type& get_allocator() const noexcept
+    allocator_type get_allocator() const noexcept(std::is_nothrow_copy_constructible<allocator_type>::value)
     {
-        return m_allocator;
-    }
-    allocator_type& get_allocator() noexcept
-    {
-        return m_allocator;
+        return allocator_traits::select_on_container_copy_construction(m_allocator);
     }
 
 public:
-    void clear() noexcept(false)
+    void clear() noexcept(std::is_nothrow_destructible<node_type>::value)
     {
-        for(node_t* node = m_first.exchange(nullptr); node != nullptr; )
+        for(node_type* node = m_first.exchange(nullptr); node != nullptr; )
         {
-            node_t* next = node->next;
+            node_type* next = node->next;
 
-            node->~node_t();
-
-            m_allocator.deallocate(node, 1);
+            node->~node_type();
 
             node = next;
         }
     }
 
 private:
-    void insert(node_t* node) noexcept
+    void put(node_type* node) noexcept
     {
-        node_t* first = m_first;
+        node_type* first = m_first;
         do
         {
             node->next = first;
@@ -169,44 +290,44 @@ private:
     }
 
 public:
-    T& add()
+    T& add() noexcept(false)
     {
-        node_t* node = m_allocator.allocate(1);
+        node_type* node = m_allocator.allocate(1);
 
-        new (node) node_t();
+        new (node) node_type();
 
-        insert(node);
+        put(node);
 
         return node->value;
     }
-    T& add(T&& value)
+    T& add(T&& value) noexcept(false)
     {
-        node_t* node = m_allocator.allocate(1);
+        node_type* node = m_allocator.allocate(1);
 
-        new (node) node_t(std::move(value));
+        new (node) node_type(std::move(value));
 
-        insert(node);
+        put(node);
 
         return node->value;
     }
-    T& add(const T& value)
+    T& add(const T& value) noexcept(false)
     {
-        node_t* node = m_allocator.allocate(1);
+        node_type* node = m_allocator.allocate(1);
 
-        new (node) node_t(value);
+        new (node) node_type(value);
 
-        insert(node);
+        put(node);
 
         return node->value;
     }
     template<typename... Args>
-    T& emplace(Args&&... args)
+    T& emplace(Args&&... args) noexcept(false)
     {
-        node_t* node = m_allocator.allocate(1);
+        node_type* node = m_allocator.allocate(1);
 
-        new (node) node_t(std::forward<Args>(args)...);
+        new (node) node_type(std::forward<Args>(args)...);
 
-        insert(node);
+        put(node);
 
         return node->value;
     }
@@ -241,9 +362,10 @@ public:
         iterator_t& operator++()    noexcept(DIAG_NOEXCEPT) {                          increment(); return *this; }
         iterator_t  operator++(int) noexcept(DIAG_NOEXCEPT) { iterator_t prev = *this; increment(); return  prev; }
     };
-    using       iterator = iterator_t<      node_t,       value_type>;
-    using const_iterator = iterator_t<const node_t, const value_type>;
 
+public:
+    using       iterator = iterator_t<      node_type,       value_type>;
+    using const_iterator = iterator_t<const node_type, const value_type>;
 public:
     const_iterator begin() const noexcept
     {
@@ -282,7 +404,7 @@ void swap(
     gkr::lockfree_grow_only_bag<T, Allocator>& lhs,
     gkr::lockfree_grow_only_bag<T, Allocator>& rhs
     )
-    noexcept
+    noexcept(gkr::lockfree_grow_only_bag<T, Allocator>::swap_is_noexcept)
 {
     lhs.swap(rhs);
 }
--- tests/t-lockfree_grow_only_bag.cpp
+++ tests/t-lockfree_grow_only_bag.cpp
@@ -1,31 +1,389 @@
 #include <gkr/lockfree_grow_only_bag.h>
 
 #include <string>
+#include <atomic>
+#include <utility>
 
 #include <catch2/catch_test_macros.hpp>
 
-struct Data
+#ifndef if_constexpr
+#ifdef __cpp_if_constexpr
+#define if_constexpr if constexpr
+#else
+#define if_constexpr if
+#endif
+#endif
+
+using namespace gkr;
+
+template<typename T>
+class PreAllocatedStorage
 {
-    int a;
-    bool b;
+    PreAllocatedStorage           (PreAllocatedStorage&&) noexcept = delete;
+    PreAllocatedStorage& operator=(PreAllocatedStorage&&) noexcept = delete;
+
+    PreAllocatedStorage           (const PreAllocatedStorage&) noexcept = delete;
+    PreAllocatedStorage& operator=(const PreAllocatedStorage&) noexcept = delete;
+
+private:
+    std::size_t m_count = 0;
+    T*          m_data  = nullptr;
+    T**         m_ptrs  = nullptr;
+
+    int m_total_allocations = 0;
+    int m_total_elements    = 0;
+
+private:
+    void make_checks()
+    {
+        CHECK(m_total_allocations == 0);
+        CHECK(m_total_elements    == 0);
+    }
+
+public:
+    PreAllocatedStorage()
+    {
+    }
+    ~PreAllocatedStorage()
+    {
+        make_checks();
+        if(m_ptrs != nullptr) delete [] m_ptrs;
+        if(m_data != nullptr) delete [] m_data;
+    }
+
+public:
+    void reset(std::size_t count = 0)
+    {
+        make_checks();
+
+        m_total_allocations = 0;
+        m_total_elements    = 0;
+
+        if(count > m_count)
+        {
+            if(m_ptrs != nullptr) delete [] m_ptrs;
+            if(m_data != nullptr) delete [] m_data;
+
+            m_data = new T [count];
+            m_ptrs = new T*[count];
+
+            m_count = count;
+        }
+    }
+
+public:
+    T* allocate(std::size_t n)
+    {
+        REQUIRE(n > 0);
+
+        for(std::size_t pos, index = 0; index < m_count; ++index)
+        {
+            for(pos = 0; pos < n; ++pos)
+            {
+                if(m_ptrs[index + pos] != nullptr) break;
+            }
+            if(pos == n)
+            {
+                m_total_allocations += 1;
+                m_total_elements    += int(n);
+
+                for( ; n-- > 0; ++index)
+                {
+                    m_ptrs[index] = m_data + index;
+                }
+                return m_data + pos;
+            }
+        }
+        throw std::bad_alloc();
+    }
+    void deallocate(T* ptr, std::size_t n)
+    {
+        std::size_t index = std::size_t(m_data - ptr);
+
+        REQUIRE((index + 0) <  m_count);
+        REQUIRE((index + n) <= m_count);
+
+        m_total_allocations -= 1;
+        m_total_elements    -= n;
+
+        for( ; n-- > 0; ++index)
+        {
+            m_ptrs[index] = nullptr;
+        }
+    }
 };
 
-TEST_CASE("container.lockfree_bag. Instantiation")
+template<typename T>
+PreAllocatedStorage<T>& get_preallocated_storage()
 {
-    gkr::lockfree_grow_only_bag<int>         bag1;
-    gkr::lockfree_grow_only_bag<Data>        bag2;
-    gkr::lockfree_grow_only_bag<std::string> bag3;
+    static thread_local PreAllocatedStorage<T> storage;
+
+    return storage;
+}
+
+enum
+{
+    EqualsNever   = 0,
+    EqualsAlways  = 16,
+    EqualsByValue = 32,
+
+    PropagatesNever            = 0,
+    PropagatesOnCopyAssignment = 1,
+    PropagatesOnMoveAssignment = 2,
+    PropagatesOnSwap           = 4,
+};
+template<typename T, int FLAGS>
+class unit_test_allocator
+{
+public:
+    using      value_type = T;
+    using       size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+
+public:
+    using is_always_equal = std::bool_constant<(FLAGS & EqualsAlways) != 0>;
+
+    using propagate_on_container_copy_assignment = std::bool_constant<(FLAGS & PropagatesOnCopyAssignment) != 0>;
+    using propagate_on_container_move_assignment = std::bool_constant<(FLAGS & PropagatesOnMoveAssignment) != 0>;
+    using propagate_on_container_swap            = std::bool_constant<(FLAGS & PropagatesOnSwap          ) != 0>;
+
+private:
+    int m_self_allocations = 0;
+    int m_self_elements    = 0;
+
+protected:
+    void reset()
+    {
+        if_constexpr(propagate_on_container_copy_assignment::value)
+        {
+            CHECK(m_self_allocations == 0);
+            CHECK(m_self_elements    == 0);
+        }
+        m_self_allocations = 0;
+        m_self_elements    = 0;
+    }
+    void swap(int&)
+    {
+    }
+
+protected:
+    unit_test_allocator() noexcept
+    {
+    }
+    ~unit_test_allocator()
+    {
+        reset();
+    }
+
+    template<typename U>
+    unit_test_allocator(const unit_test_allocator<U, FLAGS>&) noexcept
+    {
+    }
+    unit_test_allocator(const unit_test_allocator&) noexcept
+    {
+    }
+    unit_test_allocator& operator=(const unit_test_allocator&) noexcept
+    {
+        reset();
+        return *this;
+    }
+
+    unit_test_allocator(unit_test_allocator&& other) noexcept
+        : m_self_allocations(std::exchange(other.m_self_allocations, 0))
+        , m_self_elements   (std::exchange(other.m_self_elements   , 0))
+    {
+    }
+    unit_test_allocator& operator=(unit_test_allocator&& other) noexcept
+    {
+        m_self_allocations = std::exchange(other.m_self_allocations, 0);
+        m_self_elements    = std::exchange(other.m_self_elements   , 0);
+        return *this;
+    }
+
+    void swap(unit_test_allocator& other) noexcept
+    {
+        std::swap(m_self_allocations, other.m_self_allocations);
+        std::swap(m_self_elements   , other.m_self_elements);
+    }
+
+protected:
+    bool equals_by_value() const noexcept
+    {
+        return ((FLAGS & (EqualsAlways | EqualsByValue)) != 0);
+    }
+
+public:
+    [[nodiscard]]
+    T* allocate(std::size_t n)
+    {
+        return get_preallocated_storage<T>().allocate(n);
+    }
+    void deallocate(T* ptr, std::size_t n)
+    {
+        get_preallocated_storage<T>().deallocate(ptr, n);
+    }
+};
+
+#define DEFINE_ALLOCATOR(NAME, FLAGS) \
+template<class T>                                                                                       \
+class NAME : public unit_test_allocator<T, FLAGS>                                                       \
+{                                                                                                       \
+public:                                                                                                 \
+    using base_t = unit_test_allocator<T, FLAGS>;                                                       \
+   ~NAME() noexcept {}                                                                                  \
+    NAME() noexcept : base_t() {}                                                                       \
+    NAME(const NAME&  other) noexcept : base_t(          other ) {}                                     \
+    NAME(      NAME&& other) noexcept : base_t(std::move(other)) {}                                     \
+    NAME& operator=(const NAME&  other) noexcept { base_t::operator=(          other ); return *this; } \
+    NAME& operator=(      NAME&& other) noexcept { base_t::operator=(std::move(other)); return *this; } \
+    bool operator==(const NAME&) const noexcept { return  base_t::equals_by_value(); }                  \
+    bool operator!=(const NAME&) const noexcept { return !base_t::equals_by_value(); }                  \
+    template<typename U> NAME(const NAME<U>& other) : base_t(other) {}                                  \
+}
+
+struct Data
+{
+    Data() noexcept = default;
+   ~Data() noexcept = default;
+
+    Data           (const Data&) noexcept = default;
+    Data& operator=(const Data&) noexcept = default;
+
+    Data(int a_, bool b_ = false) noexcept : a(a_), b(b_)
+    {
+    }
+    Data(Data&& other) noexcept : a(std::exchange(other.a, 0)), b(std::exchange(other.b, false))
+    {
+    }
+    Data& operator=(Data&& other) noexcept
+    {
+        a = std::exchange(other.a, 0);
+        b = std::exchange(other.b, false);
+        return *this;
+    }
+    Data& operator+=(const Data& other) noexcept
+    {
+        a += other.a;
+        return *this;
+    }
+    explicit operator int() const
+    {
+        return a;
+    }
+    int  a = 0;
+    bool b = false;
+};
+
+template<typename T, typename Allocator>
+int sum(const lockfree_grow_only_bag<T, Allocator>& bag)
+{
+    T total {0};
+
+    for(auto& item : bag)
+    {
+        total += item;
+    }
+    return int(total);
+}
+
+DEFINE_ALLOCATOR(allocator1, (EqualsAlways | PropagatesNever));
+
+TEST_CASE("container.lockfree_bag. Lifecycle")
+{
+    using bag_t = lockfree_grow_only_bag<Data, allocator1<Data>>;
+
+    bag_t bag1;
 
     CHECK(bag1.empty());
-    CHECK(bag2.empty());
-    CHECK(bag3.empty());
+
+    bag1.add(Data(1));
+    bag1.add(Data(2));
+    bag1.add(Data(3));
+
+    CHECK(!bag1.empty());
+    CHECK(sum(bag1) == 6);
+
+    SECTION("copy construction")
+    {
+        bag_t bag2(bag1);
+
+        CHECK(!bag2.empty());
+        CHECK(sum(bag2) == 6);
+
+        CHECK(!bag1.empty());
+        CHECK(sum(bag1) == 6);
+    }
+    SECTION("copy assignment")
+    {
+        bag_t bag2;
+
+        bag2.add(Data(10));
+        bag2.add(Data(30));
+
+        CHECK(!bag2.empty());
+        CHECK(sum(bag2) == 30);
+
+        bag2 = bag1;
+
+        CHECK(!bag2.empty());
+        CHECK(sum(bag2) == 6);
+
+        CHECK(!bag1.empty());
+        CHECK(sum(bag1) == 6);
+    }
+    SECTION("move construction")
+    {
+        bag_t bag2(std::move(bag1));
+
+        CHECK(!bag2.empty());
+        CHECK(sum(bag2) == 6);
+
+        CHECK(bag1.empty());
+        CHECK(sum(bag1) == 0);
+    }
+    SECTION("move assignment")
+    {
+        bag_t bag2;
+
+        bag2.add(Data(10));
+        bag2.add(Data(30));
+
+        CHECK(!bag2.empty());
+        CHECK(sum(bag2) == 30);
+
+        bag2 = std::move(bag1);
+
+        CHECK(!bag2.empty());
+        CHECK(sum(bag2) == 6);
+
+        CHECK(bag1.empty());
+        CHECK(sum(bag1) == 0);
+    }
+    SECTION("swap")
+    {
+        bag_t bag2;
+
+        bag2.add(Data(10));
+        bag2.add(Data(30));
+
+        CHECK(!bag2.empty());
+        CHECK(sum(bag2) == 30);
+
+        std::swap(bag1, bag2);
+
+        CHECK(!bag2.empty());
+        CHECK(sum(bag2) == 6);
+
+        CHECK(!bag1.empty());
+        CHECK(sum(bag1) == 30);
+    }
 }
 
 TEST_CASE("container.lockfree_bag. Clear")
 {
-    gkr::lockfree_grow_only_bag<int>         bag1;
-    gkr::lockfree_grow_only_bag<Data>        bag2;
-    gkr::lockfree_grow_only_bag<std::string> bag3;
+    lockfree_grow_only_bag<int>         bag1;
+    lockfree_grow_only_bag<Data>        bag2;
+    lockfree_grow_only_bag<std::string> bag3;
 
     CHECK(bag1.empty());
     CHECK(bag2.empty());

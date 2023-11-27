65a4d334cf79ac957ec9871fb146820086af84c8
ElemagEx <dragomir.stoichev@gmail.com>
2023-11-27 20:31:20 +0200

    Fixed Linux build freeze inside MSVS

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -6,7 +6,9 @@ cd tmp
 git clone https://github.com/catchorg/Catch2.git
 cd Catch2
 @rem 3f0283de7a9c43200033da996ff9093be3ac84dc = v.3.3.2
-git checkout --detach 3f0283de7a9c43200033da996ff9093be3ac84dc
+@rem git checkout --detach 3f0283de7a9c43200033da996ff9093be3ac84dc
+@rem 6e79e682b726f524310d55dec8ddac4e9c52fb5f = v.3.4.0
+git checkout --detach 6e79e682b726f524310d55dec8ddac4e9c52fb5f
 md build
 cd build
 cmake ..
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,9 @@ cd tmp
 git clone https://github.com/catchorg/Catch2.git
 cd Catch2
 # 3f0283de7a9c43200033da996ff9093be3ac84dc = v.3.3.2
-git checkout --detach 3f0283de7a9c43200033da996ff9093be3ac84dc
+# git checkout --detach 3f0283de7a9c43200033da996ff9093be3ac84dc
+# 6e79e682b726f524310d55dec8ddac4e9c52fb5f = v.3.4.0
+git checkout --detach 6e79e682b726f524310d55dec8ddac4e9c52fb5f
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
@@ -15,3 +17,4 @@ make install
 cd ../..
 cd ..
 rm -rf tmp
+#cat install_manifest.txt | xargs echo sudo rm | sh
